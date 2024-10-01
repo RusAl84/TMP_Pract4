ae69f24a5ff6ebd0b97d4a66e45c868a7cc0f33d
Aleksey <rusal@bk.ru>
2024-10-01 10:08:09 +0300

    final

--- TMP_Pract4.cpp
+++ TMP_Pract4.cpp
@@ -1,27 +1,31 @@
 ﻿using namespace std;
-
 #include <iostream>
 
 int main()
 {
     int nop = 0;
-    int n = 10; nop++;
-    int s = 0; nop++;
-    nop++; nop++; nop++; nop++;
-    for (int i = n + 5; i < n * n; i++) {
+    int n;
+    for(int z=2; z<=30;z++){
+        nop = 0;
+        n = z; nop++;
+        int s = 0; nop++;
         nop++; nop++; nop++; nop++;
-        nop++; nop++; nop++; nop++;
-        for (int j = 5; j < n * n * n - 4; j++) {
+        for (int i = n + 5; i < n * n; i++) {
             nop++; nop++; nop++; nop++;
-            int k = 13; nop++;
-            nop++; nop++; nop++;
-            while ((2 * k + 5) < 66) {
+            nop++; nop++; nop++; nop++;
+            for (int j = 5; j < n * n * n - 4; j++) {
+                nop++; nop++; nop++; nop++;
+                int k = 13; nop++;
                 nop++; nop++; nop++;
-                k = k + 7; nop++;
-                s = s + 1; nop++;
+                while ((2 * k + 5) < 66) {
+                    nop++; nop++; nop++;
+                    k = k + 7; nop++;
+                    s = s + 1; nop++;
+                }
             }
         }
+        //cout << n << " " << nop << "\n";
+        cout << nop << "\n";
+        //cout << "S= " << s << "\n";
     }
-    cout << "S= "<< s <<"\n";
-    cout << "nop= "<< nop <<"\n";
 }
